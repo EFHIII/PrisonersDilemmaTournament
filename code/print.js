5945c0364fe44930c881ded0e1452ec83b710b8f
Edward Haas <efhiii2000@gmail.com>
2021-05-26 01:21:01 -0700

    rank

--- code/print.js
+++ code/print.js
@@ -1,11 +1,9 @@
-const fs = require("fs");
-
-let view = ['eStrats.alwaysCooperate'];
+let names = ['eStrats.alwaysCooperate'];
 
 
 for(let i=2;i<process.argv.length;i++){
   if(i==2){
-    view = [];
+    names = [];
     if(process.argv[i] == 'help'){
       console.log(
 `Prints out matches that involve any files
@@ -18,10 +16,8 @@ example: node print.js titForTat exampleStrats.simpleton
       process.exit();
     }
   }
-  view.push(process.argv[i]);
+  names.push(process.argv[i]);
 }
-
-console.log(process.argv);
 
 const data = require("./cache.json");
 
@@ -30,15 +26,38 @@ print(data);
 function print(data){
   let ans = [];
 
+  let scores={};
+
   for(let n1 in data){
+    if(!scores.hasOwnProperty(n1)){
+      scores[n1] = {
+        games:0,
+        stdev:0,
+        cum:0,
+      };
+    }
     let newest = data[n1][Object.keys(data[n1]).reduce((a, b) => a > b ? a : b)];
     for(let n2 in newest){
+      if(!scores.hasOwnProperty(n2)){
+        scores[n2] = {
+          games:0,
+          stdev:0,
+          cum:0,
+        };
+      }
       let round = newest[n2][Object.keys(newest[n2]).reduce((a, b) => a > b ? a : b)];
+
+      scores[n1].games++;
+      scores[n2].games++;
+      scores[n1].cum+=round[0];
+      scores[n2].cum+=round[1];
+      scores[n1].stdev+=round[2];
+      scores[n2].stdev+=round[3];
 
       round = round.map(a=>typeof a == 'number'?Math.round(a*100)/100:a)
 
       let has = -1;
-      view.map(a=>{
+      names.map(a=>{
         if(n1.indexOf(a)>=0){has=1}
         if(n2.indexOf(a)>=0){has=0}
       })
@@ -70,5 +89,13 @@ function print(data){
   }
 
   console.log(ans.sort((a,b)=>b[0]-a[0]).map(a=>a[1]).join(''));
-  console.log("Winners");
+
+  let winners = [];
+  for(let n in scores){
+    winners.push([n,scores[n]]);
+  }
+
+  winners.sort((a,b)=>b[1].cum/b[1].games-a[1].cum/a[1].games);
+
+  console.log(winners.map((a,b)=>`${(b+1+'').padStart(3)} | ${(a[1].cum/a[1].games+0.005+'').slice(0,4).padEnd(4,' ')} | ${a[0]}\n`).join(''));
 }
