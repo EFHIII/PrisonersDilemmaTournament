0ec4bbb54c8a6236564163fb1eecd8adc144e4e4
Edward Haas <efhiii2000@gmail.com>
2021-05-27 04:10:48 -0700

    add meta-range script

--- code/metarange.js
+++ code/metarange.js
@@ -1,20 +1,28 @@
 let weights = [];
-let TOP = Infinity;
-
+let STEPS = 200;
+let TOP = 10;
 
 for(let i=2;i<process.argv.length;i++){
   if(i==2){
     weights = [];
     if(process.argv[i] == 'help'){
       console.log(
-`Prints out standings from the meta settings defined in meta.ini
+`Prints out an animated standings interpolating between the provided meta range
 
-usage: node meta.js -top=[top] [[name]=[weight] []=[]...]
+usage: node metarange.js -top=[top] -steps=[steps] [[name]=[weightStart]-[weightEnd] []=[]-[]...]
 
-example: node meta.js titForTat=100 exampleStrats.simpleton=50
+example: node metarange.js titForTat=0-100
 `);
       process.exit();
     }
+  }
+  if(
+    process.argv[i].indexOf('-s=')==0 ||
+    process.argv[i].indexOf('-steps=')==0 ||
+    process.argv[i].indexOf('--steps=')==0
+  ){
+    STEPS = parseInt(process.argv[i].replace('--steps=','').replace('-steps=','').replace('-s=',''));
+    continue;
   }
   if(
     process.argv[i].indexOf('-t=')==0 ||
@@ -30,7 +38,7 @@ example: node meta.js titForTat=100 exampleStrats.simpleton=50
 const fs = require("fs");
 let meta = fs.readFileSync('meta.ini', 'utf8');
 
-meta = meta.split('\n').concat(weights);
+meta = meta.split('\n');
 
 meta = meta.map((a)=>{
   v = a.replace(/;.+/,'');
@@ -44,12 +52,56 @@ meta = meta.map((a)=>{
     return;
   }
 
+  //if(v[1] < 0){
+  //  v[1] = -0;
+  //}
+
   return v;
+}).filter(a=>a!=null);
+
+weights = weights.map((a)=>{
+  v = a.replace(/;.+/,'');
+  if(v.indexOf('=')<0){
+    return;
+  }
+  v = v.replace(/ /g,'').split('=');
+  v[1] = v[1].split('-');
+
+  if(v[1].length == 1){
+    v[1].push(v[1][0]);
+  }
+
+  if(v[1].length != 2){
+    return;
+  }
+
+  start = parseFloat(v[1][0]);
+  end = parseFloat(v[1][1]);
+
+  if(isNaN(start) || isNaN(end)){
+    return;
+  }
+
+  if(start < 0){
+    start = 0;
+  }
+
+  if(end < 0){
+    end = 0;
+  }
+
+  return [v[0],start,end];
 }).filter(a=>a!=null);
 
 const data = require("./cache.json");
 
-print(data, meta);
+printRange(data, meta, weights);
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 function getWeight(name, meta){
   let ans = 1;
@@ -110,6 +162,7 @@ function print(data, meta){
 
   winners.sort((a,b)=>b[1].cum/b[1].games-a[1].cum/a[1].games);
 
+  console.clear();
   console.log("  # |  avg  | stdev | weight | name\n"+
   "----+-------+-------+--------+------\n"+
   winners.filter(a=>getWeight(a[0], meta)>=0).slice(0,TOP).map((a,b)=>`${(b+1+'').padStart(3)} | `+
@@ -119,53 +172,11 @@ function print(data, meta){
   `${a[0]}\n`).join(''));
 }
 
-function oldprint(data, meta){
-  let scores={};
-
-  for(let n1 in data){
-    if(!scores.hasOwnProperty(n1)){
-      scores[n1] = {
-        games:0,
-        stdev:0,
-        cum:0,
-      };
-    }
-    let newest = data[n1][Object.keys(data[n1]).reduce((a, b) => a > b ? a : b)];
-    for(let n2 in newest){
-      if(!scores.hasOwnProperty(n2)){
-        scores[n2] = {
-          games:0,
-          stdev:0,
-          cum:0,
-        };
-      }
-      let round = newest[n2][Object.keys(newest[n2]).reduce((a, b) => a > b ? a : b)];
-
-      let weight1 = getWeight(n1,meta);
-      let weight2 = getWeight(n2,meta);
-
-      if(n1 == n2){
-        weight1-=1;
-        weight2-=1;
-      }
-
-      scores[n1].games+=weight2;
-      scores[n2].games+=weight1;
-      scores[n1].cum+=round[0]*weight2;
-      scores[n2].cum+=round[1]*weight1;
-      scores[n1].stdev+=round[2]*weight2;
-      scores[n2].stdev+=round[3]*weight1;
-    }
+async function printRange(data,meta,metaranges){
+  for(let i=0;i<STEPS;i++){
+    let tempMeta = meta.slice().concat(metaranges.map(a=>[a[0],a[1]+(a[2]-a[1])*i/STEPS]));
+    print(data,tempMeta);
+    console.log('\n'+metaranges.map(a=>`${a[0]} = ${a[1]+(a[2]-a[1])*i/STEPS}`).join('\n'));
+    await sleep(1000/30);
   }
-
-  let winners = [];
-  for(let n in scores){
-    winners.push([n,scores[n]]);
-  }
-
-  winners.sort((a,b)=>b[1].cum/b[1].games-a[1].cum/a[1].games);
-
-  console.log("  # |  avg  | stdev | weight | name");
-  console.log("----+-------+-------+--------+------");
-  console.log(winners.map((a,b)=>`${(b+1+'').padStart(3)} | ${(a[1].cum/a[1].games+0.0005+'').slice(0,5).padEnd(5,' ')} | ${(a[1].stdev/a[1].games+0.0005+'').slice(0,5).padEnd(5,' ')} | ${(getWeight(a[0], meta)+'').padStart(6)} | ${a[0]}\n`).join(''));
 }
